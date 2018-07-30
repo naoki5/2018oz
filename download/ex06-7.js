92b0e613899d0d8315baeeab13ffa723811d4d71
runandstaki <runandstaki@gmail.com>
2018-07-29 20:58:37 -0400

    message

---
+++ download/ex06-7.js
@@ -0,0 +1,7 @@
+var who='world';
+function sayhllo(){
+ alert('Hello,'+who+'!');
+}
+function someone(x){
+who=x;
+}
---
+++ download/ex6-5.js
@@ -0,0 +1,10 @@
+function sayhello(){
+var who='world';
+alert('Hello,'+who+'!');
+}
+function taro(){
+who='太郎';
+}
+function taro(){
+who='花子';
+}
